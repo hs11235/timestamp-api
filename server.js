ba5e57d2e8ba795e4b729e5487d967a1af9f5a43
Hayden Summers <haydensummers_1994@hotmail.co.uk>
2016-10-27 21:36:11 +0000

    solution

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
+'use strict';
 var express = require('express');
+var moment = require('moment');
+moment().format();
 var app = express();
 
-app.get('/', function(req, res) {
+app.get('/:time', function(req, res) {
     
-    res.send('Hello World!');
+    let time = moment(req.params.time, ['DD MMMM YYYY', 'MMMM DD YYYY', 'X']);
+    
+    if (time.isValid()) {
+        
+        res.json({
+            'unix': time.format('X'),
+            'natural': time.format('MMMM DD, YYYY')
+        });
+    } else {
+        
+        res.json({
+            'unix': null,
+            'natural': null
+        });
+    }
 });
 
 app.listen(8080, function() {
     
-    console.log('hello world sent to port 8080');
+    console.log('Listening on port 8080...');
 });
